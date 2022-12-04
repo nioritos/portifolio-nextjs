687dee095b76307d80891cdc76e16443ef6b6ba8
nioritos <junioradr2008@gmail.com>
2022-12-03 21:50:30 -0300

    coding the services area

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import deved from '../public/dev-ed-wave.png';
 
 export default function Home() {
   return (
-    <div className='py-4 px-6'>
+    <div className='py-4 px-6 dark:bg-black'>
       <Head>
         <title>Nioritos</title>
         <meta name="description" content="Nioritos's portifolio, here you will look my knowledges, projects and more about me" />
@@ -18,7 +18,7 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <header className='flex w-full h-16 justify-between items-center'>
-        <h1 className='text-black font-bold text-xl' id='logo'>nioritos</h1>
+        <h1 className=' dark:text-gray-200 text-black font-bold text-xl' id='logo'>nioritos</h1>
         <ul className='flex gap-4 justify-center items-center'>
           <li><BsFillMoonStarsFill size="24" className='hover:cursor-pointer' /></li>
           <li className='bg-teal-500 rounded-lg py-2 px-4 duration-700 hover:bg-teal-600'><Link href="" className='text-white'>Resume</Link></li>
@@ -29,13 +29,13 @@ export default function Home() {
         <section id="home" className='flex flex-row flex-wrap items-center justify-center min-h-full min-w-screen'>
           <div className="flex flex-col justify-center items-center md:items-start md:max-w-[40%] min-w-[40%] m-auto">
             <h1 className='font-bold text-teal-500 text-4xl'>Junior Guimarães</h1>
-            <h2 className='font-medium text-lg mt-2'>Web Developer and UI/UX Designer</h2>
-            <p className='text-base text-center md:text-start mt-2'>Freelancer providing services for programming and design content needs. Contact me now!</p>
+            <h2 className='font-medium text-lg mt-2 dark:text-gray-200'>Web Developer and UI/UX Designer</h2>
+            <p className='text-base text-center md:text-start mt-2 dark:text-gray-300'>Freelancer providing services for programming and design content needs. Contact me now!</p>
             <ul className='flex justify-center items-center gap-4 my-4'>
-              <li><AiFillTwitterCircle size="36" /></li>
-              <li><AiFillLinkedin size="36" /></li>
-              <li><AiFillInstagram size="36" /></li>
-              <li><AiFillYoutube size="36" /></li>
+              <li><AiFillTwitterCircle size="36" className='dark:fill-white'/></li>
+              <li><AiFillLinkedin size="36" className='dark:fill-white'/></li>
+              <li><AiFillInstagram size="36" className='dark:fill-white'/></li>
+              <li><AiFillYoutube size="36" className='dark:fill-white'/></li>
             </ul>
           </div>
           <div className="bg-gradient-to-b from-teal-500 circle rounded-full w-80 h-80 relative overflow-hidden m-auto">
@@ -43,8 +43,22 @@ export default function Home() {
           </div>
         </section>
 
-        <section id="projects" className='mt-60 flex justify-center items-center'>
-          <h1 className='text-teal-500 text-4xl font-bold'>Projects</h1>
+        <section id="services" className='mt-60 flex justify-center items-center'>
+          <div className="flex-col">
+          <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
         </section>
       </main>
     </div>
