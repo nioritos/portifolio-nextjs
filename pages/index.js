8cce4f33dbf9478826eb431de8f0f3229f7dbf0e
nioritos <junioradr2008@gmail.com>
2022-12-10 20:25:43 -0300

    details in services section was altered2313123213agora

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       <Head>
         <title>Nioritos</title>
         <meta name="description" content="Nioritos's portifolio, here you will look my knowledges, projects and more about me" />
-        <link rel="icon" href={FaviconVercel}/>
+        {/* <link rel="icon" href={FaviconVercel}/> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
