732eba5c7304eccb426242527ad744d9bf9fa44e
nioritos <junioradr2008@gmail.com>
2022-12-09 23:58:51 -0300

    asda23

--- components/services/Services.js
+++ components/services/Services.js
@@ -13,7 +13,7 @@ function Services() {
             <h3 className="text-3xl py-1 text-teal-600 font-bold">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
+              developer, I have done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
