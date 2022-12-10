3b4428facb3360733d983f57d407d84f97c0b37f
nioritos <junioradr2008@gmail.com>
2022-12-09 23:55:15 -0300

    asda2

---
+++ components/projects/Projects.js
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+
+function Projects() {
+    return(
+        <section id="project" className='mt-60 w-full flex justify-center items-center'>
+        <div className="flex flex-col w-full h-full justify-center items-center">
+          <div className='flex flex-col w-full items-center'>
+            <h3 className="text-3xl py-1 text-teal-600 font-bold">Projects</h3>
+          <h4 className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200 max-[860px]:text-left'>These're my projects, the tecnologies i used code it</h4>
+          </div>
+          <ul className=' flex flex-wrap mt-10 gap-4 max-[860px]:items-center max-[860px]:justify-center max-[1255px]:justify-center'>
+            <li className='py-10 px-5 bg-[#101010] gap-2 dark:text-white border-teal-600 border-2 rounded-lg flex flex-col '>
+              <h4 className=' text-xl font-medium'>Lorem name project</h4>
+              <p>lorem lorem lorem lorem lorem lorem projects</p>
+              <Link href="https://github.com/nioritos" className='text-teal-600 text-sm'>View more</Link>
+            </li>
+            <li className='py-10 px-5 bg-[#101010] gap-2 dark:text-white border-teal-600 border-2 rounded-lg flex flex-col '>
+              <h4 className=' text-xl font-medium'>Lorem name project</h4>
+              <p>lorem lorem lorem lorem lorem lorem projects</p>
+              <Link href="https://github.com/nioritos" className='text-teal-600 text-sm'>View more</Link>
+            </li>
+            <li className='py-10 px-5 bg-[#101010] gap-2 dark:text-white border-teal-600 border-2 rounded-lg flex flex-col '>
+              <h4 className=' text-xl font-medium'>Lorem name project</h4>
+              <p>lorem lorem lorem lorem lorem lorem projects</p>
+              <Link href="https://github.com/nioritos" className='text-teal-600 text-sm'>View more</Link>
+            </li>
+          </ul>
+          <Link href="https://github.com/nioritos" className=' mt-6 text-teal-600 text-base'>More repositories in my Github</Link>
+        
+        </div>
+      </section>
+    )
+};
+
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
+
 import { AiFillLinkedin, AiFillTwitterCircle, AiFillYoutube, AiFillInstagram } from 'react-icons/ai';
 import deved from '../public/dev-ed-wave.png';
 
 import Services from '../components/services/Services';
 import Head from '../components/head/Head';
-
+import Header from '../components/header/Header';
+import Projects from '../components/projects/Projects';
 
 export default function Home() {
   return (
     <div className='py-4 px-6 dark:bg-black'>
-    <Head/>
-      
-
+      <Head />
+      <Header />
       <main className="container min-h-full min-w-full mt-14">
         <section id="home" className='flex flex-row flex-wrap items-center justify-center min-h-full min-w-screen'>
           <div className="flex flex-col justify-center items-center md:items-start md:max-w-[40%] min-w-[40%] m-auto">
@@ -28,34 +29,11 @@ export default function Home() {
             <Link href="" className='bg-teal-500 py-2 px-20 rounded-lg mb-8'><strong>See more</strong></Link>
           </div>
           <div className="bg-gradient-to-b from-teal-500 circle rounded-full w-80 h-80 relative overflow-hidden m-auto">
-            <Image src={deved} layout="fill" objectFit="cover" />
+            <Image src={deved} layout="fill" objectFit="cover" alt='my image'/>
           </div>
-        </section>
-
-        <Services/>
-
-        <section id="project" className='mt-60 w-full flex justify-center items-center'>
-          <div className="flex flex-col w-full h-full justify-center items-center">
-            <h3 className="text-3xl py-1 text-teal-600 font-bold">project</h3>
-            <ul className='grid grid-cols-3 mt-10'>
-              <li className='p-2'>
-                <Image src="" alt="img project" className='w-full h-4' />
-                <h4>name project</h4>
-                <p>description project</p>
-              </li>
-              <li className='p-2'>
-                <Image src="" alt="img project" className='w-full h-4' />
-                <h4>name project</h4>
-                <p>description project</p>
-              </li>
-              <li className='p-2'>
-                <Image src="" alt="img project" className='w-full h-4' />
-                <h4>name project</h4>
-                <p>description project</p>
-              </li>
-            </ul>
-          </div>
-        </section>
+        </section> 
+        <Services />
+        <Projects />
       </main>
     </div>
   )
