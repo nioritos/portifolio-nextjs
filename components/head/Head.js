3a57fc8428bee73351322e6795927c9fca1d15ff
nioritos <junioradr2008@gmail.com>
2022-12-10 19:59:36 -0300

    details in services section was altered

--- components/head/Head.js
+++ components/head/Head.js
@@ -1,12 +1,11 @@
 import Head from 'next/head';
-import Favicon from '../../public/dev-ed-wave.png';
-
+import FaviconVercel from '../../public/favicon.ico';
 function HeadPage() {
     return(
         <Head>
         <title>Nioritos</title>
         <meta name="description" content="Nioritos's portifolio, here you will look my knowledges, projects and more about me" />
-        <link rel="icon" href={Favicon} />
+        <link rel="icon" href={FaviconVercel}/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
