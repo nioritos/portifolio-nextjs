44c6e517ee95a45ca517f45802963f947f228335
Junior Guimarães <77200196+nioritos@users.noreply.github.com>
2023-01-20 19:36:16 -0300

    update the favicon

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,9 @@ export default function Home() {
     <div className='py-4 px-6 dark:bg-black'>
       <Head>
         <title>Nioritos</title>
+        
         <meta name="description" content="Nioritos's portifolio, here you will look my knowledges, projects and more about me" />
-        {/* <link rel="icon" href={FaviconVercel}/> */}
+        <link rel="icon" href={deved}/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
